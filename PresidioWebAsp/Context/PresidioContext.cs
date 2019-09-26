dcb7fdc47bbaa8ef5ad09f15ef0d7a237e6737f8
Logon Aluno <logonaluno@local.com>
2019-09-26 09:52:58 -0300

    subindo presidio controller

--- PresidioWebAsp/Context/PresidioContext.cs
+++ PresidioWebAsp/Context/PresidioContext.cs
@@ -11,6 +11,7 @@ namespace PresidioWebAsp.Context
     {
 
         public DbSet<Presidiario> Presidiarios { get; set; }
+        public DbSet<Cela> Celas{ get; set; }
         public PresidioContext(DbContextOptions options)
                : base(options) { }
 
---
+++ PresidioWebAsp/Controllers/CelaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PresidioWebAsp.Context;
+using PresidioWebAsp.Models;
+
+namespace PresidioWebAsp.Controllers
+{
+    public class CelaController : Controller
+    {
+
+        public PresidioContext _context { get; set; }
+
+        public CelaController(PresidioContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Cela cela)
+        {
+            _context.Celas.Add(cela);
+            _context.SaveChanges();
+            TempData["msg"] = "Cela Cadastrada com Sucesso";
+            return Redirect("Listar");
+        }
+    }
+}
